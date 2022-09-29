d4eb82281a102ed0dfc861f2616a7566a563ccf1
KimYuRa2 <ura3118@naver.com>
2022-09-29 15:21:35 +0900

    #26. AWS RDS로 회원가입 구현 | 웹서버와 WAS | MySQL

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -5,40 +5,11 @@
 const db = require("../config/db"); 
 
 class UserStorage {
-    //#15. 컨트롤러에서 잘라서 가져옴. => 은닉화(# : 해당 변수를 public한 변수에서 private변수로 선언해줌 - 외부에서 접근할 수 없게 됨! - console.log시 undefined로 나옴)
-    // => src>databases>data>users.json으로 데이터 옮김(#21.파일에 저장하기)!
+    // #26. private한 함수들은 필요없어서 다 지워버림
 
-    //#22. 가독성을 높이기 위해 은닉화된 함수로 따로 빼기. 은닉화 된 함수는 최상단에 위치해야 함!
-    static #getUserInfo(data, id) {
-        const users = JSON.parse(data);
-        // #21. scope(범위) 때문에 fs안에 넣어줘야 함! (추가적으로, 비동기로 동작하는 것도 한가지 이유임)
-        const idx = users.id.indexOf(id);// Users.js > const { id, psword } = UserStorage.getUserInfo("test1"); 에서, "test1"라는 id의 index를 구해서 idx에 넣음
-        const userKeys = Object.keys(users); // users의 키값들만 리스트로 만듦 => [id, psword, name]
-        const userInfo = userKeys.reduce( (newUser, info) => { // reduce 메서드
-            newUser[info] = users[info][idx]; 
-            return newUser;
-        }, {});
-        return userInfo;
-    }
-
-    //#23. 
-    static #getUsers(data, isAll, fields){
-        const users = JSON.parse(data); //buffer형태의 data를 parsing해서 가져옴 
-        if(isAll) return users;
-        const newUsers = fields.reduce((newUsers, field) => { // reduce(배열메서드) - 반복문 -- fields에 대한 원소가 하나씩 순회됨.  newUsers에는 fields라는 배열의 초기값이 들어가고, 그 다음 변수들은 field에 들어오게 됨
-            // console.log(newUsers, field);
-            if(users.hasOwnProperty(field)){ //users에 field(해당하는 키 값)가 존재하는가? : 먼저 id가 있으면 그 id에 해당하는 값을 
-                newUsers[field] =  users[field];
-            }
-            return newUsers;
-        }, {}); // {}라고 쓰면 console.log(newUsers, field);찍으면 {} id , undefined psword 라고 들어감
-        console.log(newUsers);
-        return newUsers;
-    }
-
-    static getUsers(isAll, ...fields) { //static 필수(클래스 자체에서 메서드에 접근하려면 필수임) 은닉화 된 users 변수를 외부에서 쓸 수 있도록 함수로 내보내기 처리
+    // static getUsers(isAll, ...fields) { //static 필수(클래스 자체에서 메서드에 접근하려면 필수임) 은닉화 된 users 변수를 외부에서 쓸 수 있도록 함수로 내보내기 처리
         
-    }
+    // }
 
     static getUserInfo(id) { // 내가 요청한 id에 해당하는 데이터들만 가져오는 메서드
         //#25. db와 연동된 로그인 기능 만들기
@@ -55,20 +26,24 @@ class UserStorage {
        // UserStorage에서는 DB를 CRUD(생성/읽기/수정/삭제)하는 역할만 수행하고,
        // User은 해당 데이터를 가지고 검증 및 조작을 수행하도록 역할을 구분함.
         return new Promise( (resolve, reject) => { //promise 안의 db.query구문이 성공하면 resolve를 실행하고, 실패 시 reject
-            db.query(" SELECT * FROM users WHERE id = ? ", [id] , (err, data) => { //db에 쿼리 날리기!!( 쿼리문(id=?) ,[?에 넣어줄 변수], 콜백함수(err, 읽어온 data) ))
+            const query = " SELECT * FROM users WHERE id = ? ;";
+            db.query( query , [id] , (err, data) => { //db에 쿼리 날리기!!( 쿼리문(id=?) ,[?에 넣어줄 변수], 콜백함수(err, 읽어온 data) ))
                 console.log(data);
-                if(err) reject(err);
+                if(err) reject(`${err}`); // err를    ``으로 감싸서 문자열로 던짐.
                 resolve(data[0]); // 배열로 감싸져서 날아오기 떄문에 , 배열 안의 RowDataPacket만 가져오기 위해 배열의 0번지 데이터만(data[0]) 가져오도록 한다.
             });
         })
-
-       
-
     }
 
 
     static async save(userInfo){
-        
+        return new Promise( (resolve, reject) => { //promise 안의 db.query구문이 성공하면 resolve를 실행하고, 실패 시 reject
+            const query = " INSERT INTO users (id, name, psword) VALUES (?,?,?) ; ";
+            db.query( query , [ userInfo.id, userInfo.name, userInfo.psword ] , (err) => { //db에 쿼리 날리기!!( 쿼리문(id=?) ,[?에 넣어줄 변수], 콜백함수(err, 읽어온 data => 저장하는 것이기 때문에 읽어올 data없으므로 쓰지않음!) ))
+                if(err) reject(`${err}`); // err를    ``으로 감싸서 문자열로 던짐.
+                resolve( { success : true } );
+            });
+        })
     }
 
 }
